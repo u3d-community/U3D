eecc6650a24015bd31738e8aa0db1bdf26c79def
Gustavo Graña <gustavoggs@gmail.com>
2022-12-22 13:16:04 -0300

    Fix signing

--- cmake/Modules/UrhoCommon.cmake
+++ cmake/Modules/UrhoCommon.cmake
@@ -58,12 +58,13 @@ if (IOS)
     set (CMAKE_OSX_SYSROOT iphoneos)    # Set Base SDK to "Latest iOS"
     if (DEFINED ENV{CI})
         set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED "NO")
+        set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED "NO")
         set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "")
     endif ()
     # This is a CMake hack in order to make standard CMake check modules that use try_compile() internally work on iOS platform
     # The injected "flags" are not compiler flags, they are actually CMake variables meant for another CMake subprocess that builds the source file being passed in the try_compile() command
     # CAVEAT: these injected "flags" must always be kept at the end of the string variable, i.e. when adding more compiler flags later on then those new flags must be prepended in front of these flags instead
-    set (CMAKE_REQUIRED_FLAGS ";-DCMAKE_MACOSX_BUNDLE=1;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED=NO;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY=")
+    set (CMAKE_REQUIRED_FLAGS ";-DCMAKE_MACOSX_BUNDLE=1;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED=NO;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="")
     if (NOT IOS_SYSROOT)
         execute_process (COMMAND xcodebuild -version -sdk ${CMAKE_OSX_SYSROOT} Path OUTPUT_VARIABLE IOS_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)   # Obtain iOS sysroot path
         set (IOS_SYSROOT ${IOS_SYSROOT} CACHE INTERNAL "Path to iOS system root")
@@ -87,10 +88,11 @@ elseif (TVOS)
     set (CMAKE_XCODE_EFFECTIVE_PLATFORMS -appletvos -appletvsimulator)
     set (CMAKE_OSX_SYSROOT appletvos)    # Set Base SDK to "Latest tvOS"
     if (DEFINED ENV{CI})
-        set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED 0)
+        set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED "NO")
+        set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED "NO")
         set (CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "")
     endif ()
-    set (CMAKE_REQUIRED_FLAGS ";-DCMAKE_MACOSX_BUNDLE=1;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED=NO;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY=")
+    set (CMAKE_REQUIRED_FLAGS ";-DCMAKE_MACOSX_BUNDLE=1;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED=NO;-DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="")
     if (NOT TVOS_SYSROOT)
         execute_process (COMMAND xcodebuild -version -sdk ${CMAKE_OSX_SYSROOT} Path OUTPUT_VARIABLE TVOS_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)   # Obtain tvOS sysroot path
         set (TVOS_SYSROOT ${TVOS_SYSROOT} CACHE INTERNAL "Path to tvOS system root")
