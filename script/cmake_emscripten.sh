a4a5461da57d4e153df953d51ff94085cf781014
Okkoman <okkoman0xx0@gmail.com>
2024-11-13 22:48:02 +0100

    update git workflows

--- script/cmake_emscripten.sh
+++ script/cmake_emscripten.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # Copyright (c) 2008-2022 the Urho3D project.
+# Copyright (c) 2022-2024 the U3D project.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +22,22 @@
 # THE SOFTWARE.
 #
 
-$(dirname $0)/cmake_generic.sh "$@" -D WEB=1
+if [ -z "$EMSDK" ]; then
+    if [ -z "$EMSCRIPTEN_ROOT_PATH" ]; then
+        echo " "
+        echo "Error: The EMSCRIPTEN_ROOT_PATH variable is not defined."
+        echo " "
+        echo "if you don't have Emscripten installed, follow the instructions at https://emscripten.org/docs/getting_started/downloads.html"
+        echo " "
+        echo "you don't need to use emsdk_env.sh or add it to your bash profile."
+        echo "just need to set EMSCRIPTEN_ROOT_PATH (the path to emscripten sdk)."
+        echo " "
+        exit 1
+    fi
+
+    source $EMSCRIPTEN_ROOT_PATH/emsdk_env.sh
+fi
+
+emcmake $(dirname $0)/cmake_generic.sh "$@" -D WEB=1
 
 # vi: set ts=4 sw=4 expandtab:
