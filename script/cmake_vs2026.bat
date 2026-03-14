da306d9af9cf0b0799e852c11250124ed79a900a
Blue, The Magnificent <bluemagnificent@gmail.com>
2026-03-14 17:14:57 +0100

    require cmake 4.2 for only vs2026

--- script/cmake_vs2026.bat
+++ script/cmake_vs2026.bat
@@ -1,5 +1,6 @@
 ::
 :: Copyright (c) 2008-2022 the Urho3D project.
+:: Copyright (c) 2022-2026 the U3D project.
 ::
 :: Permission is hereby granted, free of charge, to any person obtaining a copy
 :: of this software and associated documentation files (the "Software"), to deal
@@ -19,5 +20,43 @@
 :: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 :: THE SOFTWARE.
 ::
+
+
+:: Configuring for Visual Studio 2026 requires minimum CMake version of 4.2. This 
+:: script ensures that the installed CMake version meets this requirement pending 
+:: when the minimum required CMake version of the overall project is bumped to 4.2 or higher.
+
+@echo off
+setlocal enabledelayedexpansion
+
+:: The minimum required version of CMake is 4.2
+set "REQ_MAJOR=4"
+set "REQ_MINOR=2"
+
+:: Get CMake version string (e.g., "cmake version 4.2.0")
+for /f "tokens=3" %%a in ('cmake --version ^| findstr /i "version"') do (
+    set "FULL_VERSION=%%a"
+)
+
+:: Split the version into Major.Minor.Patch
+for /f "tokens=1,2,3 delims=." %%a in ("%FULL_VERSION%") do (
+    set "CUR_MAJOR=%%a"
+    set "CUR_MINOR=%%b"
+)
+
+:: Logic: Check Major first, then Minor if Major is equal
+if %CUR_MAJOR% gtr %REQ_MAJOR% goto :success
+if %CUR_MAJOR% equ %REQ_MAJOR% (
+    if %CUR_MINOR% geq %REQ_MINOR% goto :success
+)
+
+:failure
+echo ---------------------------------------------------------
+echo ERROR: CMake %CUR_MAJOR%.%CUR_MINOR% found but %REQ_MAJOR%.%REQ_MINOR% or higher is required to configure U3D for Visual Studio 2026.
+echo Please update CMake and try again.
+echo ---------------------------------------------------------
+exit /b 1
+
+:success
 
 @"%~dp0cmake_generic.bat" %* -VS=18
